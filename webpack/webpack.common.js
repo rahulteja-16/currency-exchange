21ff0d0f05e72ae8705dda89bbd9e9a87065bd5b
Rahul Teja <rahulteja.a@hotmail.com>
2021-09-28 09:06:50 +0530

    Webpack env

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,70 +6,82 @@ const deps = require('../package.json').dependencies
 //
 // const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-	entry: path.resolve(__dirname, '..', './src/index.tsx'),
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|js)x?$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'babel-loader',
+const getWPConfig = (env) => {
+	return {
+		entry: path.resolve(__dirname, '..', './src/index.tsx'),
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|js)x?$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'babel-loader',
+						},
+					],
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+					type: 'asset/resource',
+				},
+				{
+					test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+					type: 'asset/  inline',
+				},
+			],
+		},
+		output: {
+			publicPath:
+				env === 'dev'
+					? 'http://localhost:3002/'
+					: 'https://currency.rahulteja.dev',
+		},
+		plugins: [
+			new HTMLWebpackPlugin({
+				template: path.resolve(__dirname, '..', './src/index.html'),
+			}),
+			new ModuleFederationPlugin({
+				name: 'currency',
+				filename: 'currency.js',
+				remotes: {
+					base:
+						env === 'dev'
+							? 'base@http://localhost:3001/base.js'
+							: 'base@https://rahulteja.dev/base.js',
+				},
+				exposes: {
+					'./Exchange': './src/Root.tsx',
+				},
+				shared: {
+					react: {
+						singleton: true,
+						requiredVersion: deps.react,
 					},
-				],
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-				type: 'asset/  inline',
-			},
+					'react-dom': {
+						singleton: true,
+						requiredVersion: deps['react-dom'],
+					},
+					'styled-components': {
+						singleton: true,
+						requiredVersion: deps['styled-components'],
+					},
+				},
+			}),
+			// new CopyPlugin({
+			// 	patterns: [{ from: '../src', to: '/build' }],
+			// }),
 		],
-	},
-	output: {
-		publicPath: 'http://localhost:3002/',
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			template: path.resolve(__dirname, '..', './src/index.html'),
-		}),
-		new ModuleFederationPlugin({
-			name: 'currency',
-			filename: 'currency.js',
-			remotes: {
-				base: 'base@http://localhost:3001/base.js',
-			},
-			exposes: {
-				'./Exchange': './src/Root.tsx',
-			},
-			shared: {
-				react: {
-					singleton: true,
-					requiredVersion: deps.react,
-				},
-				'react-dom': {
-					singleton: true,
-					requiredVersion: deps['react-dom'],
-				},
-				'styled-components': {
-					singleton: true,
-					requiredVersion: deps['styled-components'],
-				},
-			},
-		}),
-		// new CopyPlugin({
-		// 	patterns: [{ from: '../src', to: '/build' }],
-		// }),
-	],
-	stats: 'errors-only',
+		stats: 'errors-only',
+	}
+}
+
+module.exports = {
+	getWPConfig: getWPConfig,
 }
